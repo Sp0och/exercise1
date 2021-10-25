d585f08aac57f606f45809351efbed18e1c28c3b
Spooch <fierz.lasse@gmail.com>
2021-10-25 11:03:49 +0200

    Finished Harris Corner Detection

--- part2/1_harris.py
+++ part2/1_harris.py
@@ -13,10 +13,30 @@ def nonmax_suppression(harris_resp, thr, halfwidth=2):
     # 2) cornersx: list with respective column coordinates of identified corner pixels.
     # Elements from the two lists with the same index must correspond to the same corner.
 
+    h, w = im.shape
     cornersy = []
     cornersx = []
-    h, w = im.shape[:2]
-    # TODO: perform non-maximum suppression
+    for i in range(h):
+        for j in range(w):
+            max = True
+            #even consider this pixel as maximum?
+            if(harris_resp[i,j]>thr):
+                #neighbour maximum suppression
+                if(i > 0 and harris_resp[i-1,j] > harris_resp[i,j]):
+                    max = False
+                if(i < h-1 and harris_resp[i+1,j] > harris_resp[i,j]):
+                    max = False
+                if(j > 0 and harris_resp[i,j-1] > harris_resp[i,j]):
+                    max = False
+                if(j < w-1 and harris_resp[i,j+1] > harris_resp[i,j]):
+                    max = False
+            else:
+                max = False
+            if(max):
+                cornersy.append(i)
+                cornersx.append(j)
+    
+    
 
     return cornersy, cornersx
 
@@ -38,8 +58,12 @@ im = im.astype('float')
 if rot_angle != 0:
     im = rotate(im, rot_angle)
 
+w,h = im.shape
+
 # TODO: Implement Harris corners
 # Useful functions: gaussian_filter1d, gaussian_filter
+
+
 
 I_x = gaussian_filter1d(im,sigma_d,-1,1)
 I_y = gaussian_filter1d(im,sigma_d,0,1)
@@ -51,8 +75,16 @@ I_x2_s = gaussian_filter(im,sigma_w)
 I_y2_s = gaussian_filter(im,sigma_w)
 I_xy_s = gaussian_filter(I_xy,sigma_w)
 
-A = [[I_x2_s,I_xy_s],[I_xy_s,I_y2_s]]
-H = np.linalg.det(A) - kappa*np.trace(A)
+#define individual A
+def build_H_ij(I_x2_s,I_y2_s,I_xy_s,i,j):
+    Aij = [[I_x2_s[i,j],I_xy_s[i,j]],[I_xy_s[i,j],I_y2_s[i,j]]]
+    return np.linalg.det(Aij) - kappa*np.trace(Aij)**2
+
+H = np.ndarray([h,w])
+for i in range(h):
+    for j in range (w):
+        H[i,j] = build_H_ij(I_x2_s,I_y2_s,I_xy_s,i,j)
+
 corn = nonmax_suppression(H,thresh,2)
 
 
@@ -71,7 +103,7 @@ ax_arr[1].imshow(H, cmap='gray')
 ax_arr[2].imshow(im, cmap='gray')
 ax_arr[2].scatter(x=corn[1], y=corn[0], c='r', s=10)
 plt.show()
-plt.pause(5)
+plt.pause(15)
 
 
 
